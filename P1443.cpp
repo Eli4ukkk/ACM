fc9e6a3a7ccc248fdea0991012f179eb54af35ad
Eli4ukkk <2798792983@qq.com>
2026-01-20 23:30:39 +0800

    Add files via upload
    掌握queue对为什么与BFS完美契合

---
+++ P1443.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int ans[400][400];
+int n, m, x, y;
+int dx[8] = {-2, -2, -1, 1, 2, 2, -1, 1};
+int dy[8] = {-1, 1, 2, 2, -1, 1, -2, -2};
+struct Node {
+	int x_, y_;
+	int step;
+};
+void BFS() {
+	ans[x][y] = 0;
+	Node starNode = {x, y, 0};
+	queue<Node> q;
+	q.push(starNode);
+	while (!q.empty()) {
+		Node tmp = q.front();
+		q.pop();
+		for (int k = 0; k < 8; k++) {
+			int nx = tmp.x_ + dx[k];
+			int ny = tmp.y_ + dy[k];
+			if (
+			    nx >= 0 && nx < n &&
+			    ny >= 0 && ny < m &&
+			    ans[nx][ny] == -1
+			) {
+				//cout<<"访问坐标:( "<<nx<<" , "<<ny<<" )"<<endl;
+				ans[nx][ny] = tmp.step;
+				Node newNode = {nx, ny, tmp.step + 1};
+				q.push(newNode);
+			}
+		}
+	}
+}
+int main() {
+	cin >> n >> m >> x >> y;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			ans[i][j] = -1;
+		}
+	}
+	x--;
+	y--;
+	BFS();
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << ans[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+// x x x
+// x x x
+// x x x
